91b7ca6963160ec75381153feac90b5d66aaf976
Emjob <jobusethan@gmail.com>
2024-11-11 15:08:57 +0000

    Added layer mask to wall spawn

--- Assets/Scripts/Room_Spawner.cs
+++ Assets/Scripts/Room_Spawner.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Room_Spawner : MonoBehaviour
 {
     public GameObject[] rooms;
+    private int rng;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(rooms[Random.Range(0, rooms.Length)], transform.position,transform.rotation);
+        rng = Random.Range(0, rooms.Length);
+        Instantiate(rooms[rng], transform.position, Quaternion.identity);
+        //rooms[rng].transform.GetChild(0).GetComponent<Wall_Generator>().SpawnWalls();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Wall_Generator.cs
+++ Assets/Scripts/Wall_Generator.cs
@@ -13,33 +13,46 @@ public class Wall_Generator : MonoBehaviour
     public Transform backWall;
     public Transform frontWall;
 
+    [SerializeField] private LayerMask wallCheck;
+
     // Start is called before the first frame update
     void Start()
     {
-        Ray leftRay = new Ray (transform.position,Vector3.left);
+
+        StartCoroutine(Delay());
+
+        
+    }
+
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(2);
+        Ray leftRay = new Ray(transform.position, Vector3.left);
         Ray rightRay = new Ray(transform.position, Vector3.right);
         Ray forwardRay = new Ray(transform.position, Vector3.forward);
         Ray backRay = new Ray(transform.position, Vector3.back);
 
         RaycastHit hit;
 
-        if(Physics.Raycast(leftRay,out hit,100f))
+        if (Physics.Raycast(leftRay, out hit, 1000f, wallCheck))
         {
-            if(hit.collider.gameObject.tag == "Normal" )
-            {    
-                Instantiate(entranceWall,leftWall.position,leftWall.rotation);
+            if (hit.collider.gameObject.tag == "Normal")
+            {
+                Instantiate(entranceWall, leftWall.position, leftWall.rotation);
             }
-            
+
         }
         else
         {
             Debug.Log("hELLo");
             Instantiate(normalWall, leftWall.position, leftWall.rotation);
         }
-        if (Physics.Raycast(rightRay, out hit,100f))
+
+        if (Physics.Raycast(rightRay, out hit, 1000f,wallCheck))
         {
             if (hit.collider.gameObject.tag == "Normal")
-            {               
+            {
                 Instantiate(entranceWall, rightWall.position, rightWall.rotation);
             }
         }
@@ -47,10 +60,11 @@ public class Wall_Generator : MonoBehaviour
         {
             Instantiate(normalWall, rightWall.position, rightWall.rotation);
         }
-        if (Physics.Raycast(forwardRay, out hit, 100f))
+
+        if (Physics.Raycast(forwardRay, out hit, 1000f, wallCheck))
         {
             if (hit.collider.gameObject.tag == "Normal")
-            {              
+            {
                 Instantiate(entranceWall, frontWall.position, frontWall.rotation);
             }
         }
@@ -58,10 +72,11 @@ public class Wall_Generator : MonoBehaviour
         {
             Instantiate(normalWall, frontWall.position, frontWall.rotation);
         }
-        if (Physics.Raycast(backRay, out hit,100f))
+
+        if (Physics.Raycast(backRay, out hit, 1000f, wallCheck))
         {
             if (hit.collider.gameObject.tag == "Normal")
-            {             
+            {
                 Instantiate(entranceWall, backWall.position, backWall.rotation);
             }
         }
@@ -69,11 +84,11 @@ public class Wall_Generator : MonoBehaviour
         {
             Instantiate(normalWall, backWall.position, backWall.rotation);
         }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnWalls()
     {
-        
+
     }
 }
