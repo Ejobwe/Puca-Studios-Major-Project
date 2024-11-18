80efb63a67870ffbbef7ecdf850f218a6849e456
Emjob <jobusethan@gmail.com>
2024-11-18 16:20:34 +0000

    Random test UI

--- Assets/Scripts/Player_Health.cs
+++ Assets/Scripts/Player_Health.cs
@@ -1,34 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player_Health : MonoBehaviour
-{
-    // Start is called before the first frame update
 
-    public int maxHealth;
-    [SerializeField] private int currentHealth;
+    public class Player_Health : MonoBehaviour
+    {
+        // Start is called before the first frame update
+
+        public int maxHealth;
+        [SerializeField] private int currentHealth;
+
+        [SerializeField] private Image[] hearts;
+
+        public Sprite full;
+        public Sprite empty;
 
     void Start()
-    {
-        currentHealth = maxHealth;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (currentHealth <= 0)
         {
-            Destroy(gameObject);
+            currentHealth = maxHealth;
         }
 
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-    }
+        // Update is called once per frame
+        void Update()
+        {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                hearts[i].sprite = full;
+            }
+            else
+            {
+                hearts[i].sprite = empty;
+            }
+            if (i < maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
 
-    public void takeDamage(int damage)
-    {
-        if(currentHealth > 0) { 
-            currentHealth -= damage;
+        if (currentHealth <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        }
+
+        public void takeDamage(int damage)
+        {
+            if (currentHealth > 0)
+            {
+                currentHealth -= damage;
+            }
         }
     }
-}
+
