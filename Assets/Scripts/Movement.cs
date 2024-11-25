5deb87defa21cdf71ef57cf268d33d92cb8c1e0c
Emjob <jobusethan@gmail.com>
2024-11-25 13:36:30 +0000

    Version Fix

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -36,7 +36,11 @@ public class Movement : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             rb.linearVelocity = new Vector3(input.x * playerSpeed, 0, input.z * playerSpeed);
+=======
+            rb.velocity = new Vector3(input.x * playerSpeed, 0, input.z * playerSpeed);
+>>>>>>> parent of 076a66e (Merge branch 'main' of https://github.com/Ejobwe/Puca-Studios-Major-Project)
 =======
             rb.velocity = new Vector3(input.x * playerSpeed, 0, input.z * playerSpeed);
 >>>>>>> parent of 076a66e (Merge branch 'main' of https://github.com/Ejobwe/Puca-Studios-Major-Project)
