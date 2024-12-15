3da74fad75709802055d04430e0928bb383e0bac
genericvillager <seanjack1014@gmail.com>
2024-12-15 16:12:59 +0000

    Main Menu Progress

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject canvas;
+    //[SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject MainMenu;
     [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject ControlsMenu;
+    [SerializeField] private GameObject MusicMenu;
     [SerializeField] private GameObject pause;
     [SerializeField] private bool paused;
     void Awake()
     {
         pause.SetActive(false);
         settings.SetActive(false);
+        ControlsMenu.SetActive(false);
+        MusicMenu.SetActive(false);
     }
 
     void Update()
@@ -40,9 +46,31 @@ public class MenuManager : MonoBehaviour
         }
     }
 
+    public void Resume()
+    {
+        //canvas.SetActive(false);
+        settings.SetActive(false);
+        pause.SetActive(false);
+        paused = false;
+    }
+
+    public void NewRun()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void Settings()
     {
         settings.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+
+    public void Back()
+    {
+        settings.SetActive(false);
+        MusicMenu.SetActive(false);
+        ControlsMenu.SetActive(false);
+        MainMenu.SetActive(true);
     }
 
     public void Exit()
