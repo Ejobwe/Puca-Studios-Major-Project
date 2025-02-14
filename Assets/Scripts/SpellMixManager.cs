c8877f361c7896d4305e1c8089db3b41cbafa017
Mixer-one <99894586+Mixer-one@users.noreply.github.com>
2025-02-14 03:36:28 +0000

    We(almost) made steam

---
+++ Assets/Scripts/SpellMixManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class SpellMixManager : MonoBehaviour
+{
+    public  List<GameObject>  Spells = new List<GameObject>();
+    public List<SpellProperty> Props = new List<SpellProperty>();
+    private int oldcount;
+    void Start()
+    {
+        StartCoroutine(Flush());
+    }
+
+    
+    void Update()
+    {
+        oldcount = Spells.Count;
+        for (int i = 0; i < Spells.Count; i++)
+        {
+            for (int j = 0; j < Spells.Count; j++)
+            {
+                if (Props[i].combo ==Spells[j])
+                {
+                    SpellFuse(Props[i],Props[j]);
+                }
+            }
+        }
+    }
+
+    private void SpellFuse(SpellProperty SP1, SpellProperty SP2)
+    {
+        if (SP1.Burning || SP2.Burning && SP1.Wet || SP2.Wet)
+        {
+            Debug.Log("WE MADE STEAM");
+        }
+    }
+    #region Flush
+
+    
+
+    
+    private IEnumerator Flush()
+    {
+        for (int i = 0; i < Spells.Count; i++)
+        {
+            if (null == Spells[i])
+            {
+                Spells.Remove(Spells[i]);
+            }
+        }
+        for (int i = 0; i < Props.Count; i++)
+        {
+            if (Props[i] == null)
+            {
+                Props.Remove(Props[i]);
+            }
+        }
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(Flush());
+    }
+    #endregion
+    
+}
---
+++ Assets/Scripts/SpellProperty.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpellProperty : MonoBehaviour
+{
+    [SerializeField] private SpellMixManager SMM;
+    public bool Burning;
+    public bool Wet;
+    public GameObject combo;
+
+void Start()
+    {
+        SMM = GameObject.Find("GameManager").GetComponent<SpellMixManager>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<SpellProperty>())
+        {
+            SMM.Spells.Add(this.GameObject());
+            SMM.Props.Add(this);
+            combo = other.GameObject();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SMM.Spells.Remove(this.GameObject());
+        SMM.Props.Remove(this);
+    }
+}
