c8cc384917c7c9b56de6d062e556733f5271847e
Ejobwe <jobusethan@gmail.com>
2025-03-18 15:24:46 +0000

    ?

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -19,26 +19,29 @@ public class EnemySpawner : MonoBehaviour
 
     void Spawn()
     {
-        Waves[0].SetActive(true);
-        if(Waves[0].transform.childCount == 0 && Waves[1] != null)
+        if (Waves[0] != null)
         {
-            Waves[1].SetActive(true);
-        }
-        else
-        {
-            gameObject.transform.parent.transform.GetComponent<RoomStart>().roomFinished = true;
-        }
-        if (Waves[1].transform.childCount == 0 && Waves[2] != null)
-        {
-            Waves[2].SetActive(true);
-        }
-        else
-        {
-            gameObject.transform.parent.transform.GetComponent<RoomStart>().roomFinished = true;
-        }
-        if (Waves[2].transform.childCount == 0)
-        {
-            gameObject.transform.parent.transform.GetComponent<RoomStart>().roomFinished = true;
+            Waves[0].SetActive(true);
+            if (Waves[0].transform.childCount == 0 && Waves[1] != null)
+            {
+                Waves[1].SetActive(true);
+            }
+            else
+            {
+                gameObject.transform.parent.transform.GetComponent<RoomStart>().roomFinished = true;
+            }
+            if (Waves[1].transform.childCount == 0 && Waves[2] != null)
+            {
+                Waves[2].SetActive(true);
+            }
+            else
+            {
+                gameObject.transform.parent.transform.GetComponent<RoomStart>().roomFinished = true;
+            }
+            if (Waves[2].transform.childCount == 0)
+            {
+                gameObject.transform.parent.transform.GetComponent<RoomStart>().roomFinished = true;
+            }
         }
     }
 }
