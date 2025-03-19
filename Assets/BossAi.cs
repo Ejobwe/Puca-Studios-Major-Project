992fbd0e83a3fba877da43a1cebf6d9fb5dc3a51
Ejobwe <jobusethan@gmail.com>
2025-03-19 12:58:15 +0000

    Boss Ai working

--- Assets/BossAi.cs
+++ Assets/BossAi.cs
@@ -6,11 +6,14 @@ public class BossAi : MonoBehaviour
 {
     public Animator moves;
 
+    private Quaternion origin;
+
     private bool attacking;
     // Start is called before the first frame update
     void Start()
     {
         attacking = false;
+        origin = gameObject.transform.rotation;
     }
 
     // Update is called once per frame
@@ -26,6 +29,7 @@ public class BossAi : MonoBehaviour
     IEnumerator Attack()
     {
         int i = Random.Range(1, 3);
+        gameObject.transform.rotation = origin;
         yield return new WaitForSeconds(3);
 
         if(i == 1)
@@ -49,7 +53,7 @@ public class BossAi : MonoBehaviour
     IEnumerator Slam()
     {
         moves.SetBool("Slam", true);
-
+        transform.LookAt(new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x, this.transform.position.y, GameObject.FindGameObjectWithTag("Player").transform.position.z));
         yield return new WaitForSeconds(1);
         moves.SetBool("Slam", false);
         attacking = false;
