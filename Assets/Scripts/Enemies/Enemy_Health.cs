3fa6425f886e2923483a775584690c3d6f0d94a7
genericvillager <seanjack1014@gmail.com>
2025-03-19 22:42:04 +0000

    Enemy health updated

--- Assets/Scripts/Enemies/Enemy_Health.cs
+++ Assets/Scripts/Enemies/Enemy_Health.cs
@@ -20,7 +20,7 @@ public class Enemy_Health : MonoBehaviour
     {
         if (currentHealth <= 0 && !boss)
         {
-            gameObject.SetActive(false);
+            Destroy(gameObject);
         }
         if (currentHealth <= 0 && boss)
         {
